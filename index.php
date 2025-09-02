1f3c4a38673ce1c8f908e0aebaffa8385704a745
Relina Russak <relinarussak@gmail.com>
2025-09-02 11:31:53 +0300

    Add array examples

--- index.php
+++ index.php
@@ -1,27 +1,42 @@
 <?php
-
-echo "Hello World!\n";
-echo ("Hello World!\n");
-
-$var = 1;
-echo $var;
-echo 'Var is $var';
-echo "\nVar is $var\n";
-echo $var . "\n";
-$num = 1;
-$num = $num + 1;
-$num++;
-$num--;
-$num += 1;
-$num -= 1;
-$num *= 2;
-$num /= 3;
-$num = 13;
-$num %= 5;
-$num = 1.3;
-$num **= 2;
-$num = true;
-$num = false;
-$bar;
-$bar = NULL;
-var_dump($bar);
+$array = array(1, 2, 3, 4);
+$array = [1, 2, 3, 4];
+$array = [
+    1, 
+    'hello',
+    true,
+    NULL,
+    [1, 2, 3]
+];
+$array = [
+    'name' => 'Relina',
+    'age' => 17,
+    'gender' => 'female',
+    'isCool' => true
+];
+$array = [
+    6 => 'lol',
+    1,
+    2,
+    3,
+    'name' => 'Relina',
+    'age' => 17,
+    1000 => 88,
+    'hello world',
+    'gender' => 'female',
+    'isCool' => true,
+    4,
+    5,
+];
+var_dump($array[7]);
+$array = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+var_dump($array[2][1]);
+$array = [1, 2, 3];
+array_push($array, 4);
+array_push($array, 5, 6, 7);
+$array[] = 8;
+var_dump($array);
