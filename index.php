e9ae45b5def6c0e2d281ef47c394faa75e5fb463
Relina Russak <relinarussak@gmail.com>
2025-09-09 10:32:46 +0300

    add if

--- index.php
+++ index.php
@@ -1,13 +1,40 @@
 <?php
- 
-$bool = true && false;
-$bool = true || false;
-$bool = !false;
-$bool = (true  && false || false) && !(true || false && true);
-$bool = 10 > 5;
-$bool = 10 < 5;
-$bool = 10 < 10;
-$bool = 10 == '10';
-$bool = 10 !== '10';
-$bool = 10 === '10';  // === controlls the types first
-var_dump($bool);
+$num = 15;
+if($num = 10) {
+    var_dump('Suurem');
+} elseif ($num < 10) {
+
+} else {
+
+}
+
+$day = date('w', strtotime('09.09.2025'));
+var_dump($day);
+
+switch($day) {
+    case 1:
+        var_dump('Esmaspäev');
+        break;
+    case 2:
+        var_dump('Teisipäev');
+        break;
+    case 3:
+        var_dump('Kolmapäev');
+        break;
+    case 4:
+        var_dump('Neljapäev');
+        break;
+    case 5:
+        var_dump('Õhtul');
+        break;
+    case 6:
+        var_dump('Pidupäev');
+        break;
+    case 0:
+        var_dump('Pühapäev');
+        break;
+    default:
+        var_dump('imelik');
+        break;
+
+}
