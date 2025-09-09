b11bc7e9f42836dbe5fd9e770c8811421809330f
Relina Russak <relinarussak@gmail.com>
2025-09-09 10:22:39 +0300

    add boolean

--- index.php
+++ index.php
@@ -1,42 +1,13 @@
 <?php
-$array = array(1, 2, 3, 4);
-$array = [1, 2, 3, 4];
-$array = [
-    1, 
-    'hello',
-    true,
-    NULL,
-    [1, 2, 3]
-];
-$array = [
-    'name' => 'Relina',
-    'age' => 17,
-    'gender' => 'female',
-    'isCool' => true
-];
-$array = [
-    6 => 'lol',
-    1,
-    2,
-    3,
-    'name' => 'Relina',
-    'age' => 17,
-    1000 => 88,
-    'hello world',
-    'gender' => 'female',
-    'isCool' => true,
-    4,
-    5,
-];
-var_dump($array[7]);
-$array = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-];
-var_dump($array[2][1]);
-$array = [1, 2, 3];
-array_push($array, 4);
-array_push($array, 5, 6, 7);
-$array[] = 8;
-var_dump($array);
+ 
+$bool = true && false;
+$bool = true || false;
+$bool = !false;
+$bool = (true  && false || false) && !(true || false && true);
+$bool = 10 > 5;
+$bool = 10 < 5;
+$bool = 10 < 10;
+$bool = 10 == '10';
+$bool = 10 !== '10';
+$bool = 10 === '10';  // === controlls the types first
+var_dump($bool);
