e46cab5f276a1256201f600d4e63cd0c326620b6
Relina Russak <relinarussak@gmail.com>
2025-09-09 13:05:48 +0300

    Add visibility examples

--- index.php
+++ index.php
@@ -1,22 +1,51 @@
 <?php
- 
- class Box{
-    public $width;
-    public $height;
-    public $length;
-    
-public function __construct($w, $h, $l){
-    $this->width = $w;
-    $this->height = $h;
-    $this->length = $l;
-}
-    public function volume(){
-        return $this->width * $this->height * $this->length;
-       }
- }
 
-$box1 = new Box();
-$box1->width = 10;
-$box2 = $box1;
-$box2->width = 20;
-var_dump($box1->width);
+class Box {
+    private $width;
+    protected $heigth;
+    private $length;
+
+    public function __construct($w=0, $h=0, $l=0)
+    {   
+    public function __construct($w=0, $h=0, $l=0)
+        $this->length = $l;
+    }
+
+    public function getWidth(){
+        return $this->width;
+    }
+
+    public function setWidth($width){
+        if($width > 0 && is_numeric($width)){
+            $this->width = $width;
+        } else {
+            throw new Exception('You are stupid dumbass');
+        }
+
+        
+    }
+
+    public function volume(){
+        return $this->width * $this->heigth * $this->length;
+    }
+}
+
+class MetalBox extends Box {
+    public $material = 'metal🤘';
+    public $massPerUnit = 2; 
+    
+    public function changeWidth(){
+        $this->width = 333;
+    }
+
+    public function mass() {
+        return $this->volume() * $this->massPerUnit;
+    }
+}
+
+$metalBox = new Box(2,3,4);
+$metalBox->setWidth(12);
+
+
+var_dump($metalBox);
+var_dump($metalBox->volume());
