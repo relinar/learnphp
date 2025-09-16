6b80573d5d7b9aec4bccf9cd1b8a3b7c159afc29
Relina Russak <relinarussak@gmail.com>
2025-09-16 11:21:51 +0300

    Old site example

--- index.php
+++ index.php
@@ -1,4 +1,49 @@
 <?php
+$posts = [
+  [
+    'title' => 'World news 1',
+    'published' => '16.09.2025',
+    'author' => 'Kaspar',
+    'body' => 'Some world news body 1'
+  ],
+  [
+    'title' => 'World news 2',
+    'published' => '15.09.2025',
+    'author' => 'Martin',
+    'body' => 'Some world news body 2'
+  ],
+  [
+    'title' => 'World news 3',
+    'published' => '14.09.2025',
+    'author' => 'Pets',
+    'body' => 'Some world news body 3'
+  ],
+  [
+    'title' => 'World news 4',
+    'published' => '13.09.2025',
+    'author' => 'Kelly',
+    'body' => 'Some world news body 4'
+  ],
+];
+?>
+
+
+<?php include __DIR__ . '/partials/header.php'; ?>
+    <main class="container">
+      <?php include __DIR__ . '/partials/hero.php'; ?>
+      <?php include __DIR__ . '/partials/featured.php'; ?>
+
+      <div class="row g-5">
+        <div class="col-md-8">
+            <?php include __DIR__ . '/partials/posts.php'; ?>
+        </div>
+        <div class="col-md-4">
+          <?php include __DIR__ . '/partials/sidebar.php'; ?>
+        </div>
+      </div>
+    </main>
+<?php include __DIR__ . '/partials/footer.php'; ?>
+<?php
 
 class Cat {
     use Colorful;
