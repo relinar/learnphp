0c22661fa198ee2525fcd586b43957188f29cf7a
Relina Russak <relinarussak@gmail.com>
2025-09-26 12:26:26 +0300

    Add better router

--- public/index.php
+++ public/index.php
@@ -15,81 +15,74 @@ spl_autoload_register(function ($class){
 });
 
 
+use App\Router;
 
 
 
-// require_once __DIR__ . '/../src/Router.php';
-// require_once __DIR__ . '/../src/DB.php';
-use App\Controllers\PublicController as PC;
+Router::addRoute('/', function () {
+        $posts = [
+            [
+                'title' => 'World news 1',
+                'published' => '16.09.2025',
+                'author' => 'Kaspar',
+                'body' => 'Some world news body 1'
+            ],
+            [
+                'title' => 'World news 2',
+                'published' => '15.09.2025',
+                'author' => 'Martin',
+                'body' => 'Some world news body 2'
+            ],
+            [
+                'title' => 'World news 3',
+                'published' => '14.09.2025',
+                'author' => 'Pets',
+                'body' => 'Some world news body 3'
+            ],
+            [
+                'title' => 'World news 4',
+                'published' => '13.09.2025',
+                'author' => 'Kelly',
+                'body' => 'Some world news body 4'
+            ],
+        ];
+        include __DIR__ . '/../views/index.php';
+});
 
+Router::addRoute('/us', function () {
+        $posts = [
+            [
+                'title' => 'U.S news 1',
+                'published' => '16.09.2025',
+                'author' => 'Kaspar',
+                'body' => 'Some U.S news body 1'
+            ],
+            [
+                'title' => 'U.S news 2',
+                'published' => '15.09.2025',
+                'author' => 'Martin',
+                'body' => 'Some U.S news body 2'
+            ],
+            [
+                'title' => 'U.S news 3',
+                'published' => '14.09.2025',
+                'author' => 'Pets',
+                'body' => 'Some U.S news body 3'
+            ],
+            [
+                'title' => 'U.S news 4',
+                'published' => '13.09.2025',
+                'author' => 'Kelly',
+                'body' => 'Some U.S news body 4'
+            ],
+        ];
+        include __DIR__ . '/../views/us.php';
+});
 
-$router = new App\Router();
-dump($router);
-$db = new App\DB();
-dump($db);
-$controller = new PC();
-dump($controller);
-
-
-// switch ($_SERVER['REQUEST_URI']) {
-//     case '/':
-//         $posts = [
-//             [
-//                 'title' => 'World news 1',
-//                 'published' => '16.09.2025',
-//                 'author' => 'Kaspar',
-//                 'body' => 'Some world news body 1'
-//             ],
-//             [
-//                 'title' => 'World news 2',
-//                 'published' => '15.09.2025',
-//                 'author' => 'Martin',
-//                 'body' => 'Some world news body 2'
-//             ],
-//             [
-//                 'title' => 'World news 3',
-//                 'published' => '14.09.2025',
-//                 'author' => 'Pets',
-//                 'body' => 'Some world news body 3'
-//             ],
-//             [
-//                 'title' => 'World news 4',
-//                 'published' => '13.09.2025',
-//                 'author' => 'Kelly',
-//                 'body' => 'Some world news body 4'
-//             ],
-//         ];
-//         include __DIR__ . '/../views/index.php';
-//         break;
-//     case '/us':
-//         $posts = [
-//             [
-//                 'title' => 'U.S news 1',
-//                 'published' => '16.09.2025',
-//                 'author' => 'Kaspar',
-//                 'body' => 'Some U.S news body 1'
-//             ],
-//             [
-//                 'title' => 'U.S news 2',
-//                 'published' => '15.09.2025',
-//                 'author' => 'Martin',
-//                 'body' => 'Some U.S news body 2'
-//             ],
-//             [
-//                 'title' => 'U.S news 3',
-//                 'published' => '14.09.2025',
-//                 'author' => 'Pets',
-//                 'body' => 'Some U.S news body 3'
-//             ],
-//             [
-//                 'title' => 'U.S news 4',
-//                 'published' => '13.09.2025',
-//                 'author' => 'Kelly',
-//                 'body' => 'Some U.S news body 4'
-//             ],
-//         ];
-//         include __DIR__ . '/../views/us.php';
-//         break;
-//     default:
-//         echo '404 page not found';
-// }
+$router = new App\Router($_SERVER['REQUEST_URI']);
+$match = $router->match();
+if($match){
+    call_user_func($match['action']);
+} else {
+    echo '<img src="https://http.cat/404">';
+}
--- src/Router.php
+++ src/Router.php
@@ -3,5 +3,29 @@
 namespace App;
 
 class Router {
+    private static $routes = [];
 
+    private $path;
+
+    public function __construct($path)
+    {
+        $this->path = $path;
+    }
+
+    public function match() {
+        foreach(self::$routes as $route) {
+            if($route['path'] === $this->path){
+                return $route;
+            }
+        }
+        return false;
+    }
+
+    public static function getRoutes(){
+        return self::$routes;
+    }
+
+    public static function addRoute($path, $action) {
+        self::$routes[] = ['path' => $path, 'action' => $action];
+    }
 }
